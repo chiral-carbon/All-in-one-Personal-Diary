13dd3bdb87bccac5e6fd946c97fb1e1d0d99da96
Medha Sawhney <sawhney.medha@gmail.com>
2019-03-31 20:08:19 +0530

    Adding study drill

--- Learning-Ruby/ZedEShawBook/Exercise11.rb
+++ Learning-Ruby/ZedEShawBook/Exercise11.rb
@@ -6,3 +6,12 @@ print "How much do you weigh? "
 weight = gets.chomp
 
 puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+#print instead of puts to print the string without a \n (newline)
+
+#Study Drill
+
+print "How old are you"
+age =gets.chomp.to_i
+ puts "You are #{age} years old and you will be #{age+10} years old after 10 years."
+ 
