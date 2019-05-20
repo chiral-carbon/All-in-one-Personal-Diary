7e24f004913df7256e6ef33636e6a3886b42919b
Medha Sawhney <sawhney.medha@gmail.com>
2019-05-20 23:41:36 +0530

    Updating hello and helo html views.
    Signed-off-by: Medha Sawhney <sawhney.medha@gmail.com>

--- Learning-RubyOnRails/environment/sample_app/app/controllers/application_controller.rb
+++ Learning-RubyOnRails/environment/sample_app/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exceptiondef hello
+  protect_from_forgery with: :exception
+  def hello
     render html: "hello, world!"
   end
   def hello
